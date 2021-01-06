63de6498d189167a97768a7b09c09d00c5121f64
Taylor Dean <taylor@makeshift.dev>
2021-01-06 11:09:59 -0500

    Add gettext() function

--- gettext.go
+++ gettext.go
@@ -7,57 +7,6 @@ import (
 	"github.com/taylor-s-dean/gogettext/po2json"
 )
 
-/*
-{
-    "": {
-        "": {
-            "Content-Transfer-Encoding": "8bit",
-            "Content-Type": "text/plain; charset=UTF-8",
-            "Language": "ru",
-            "MIME-Version": "1.0",
-            "Plural-Forms": "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);"
-        },
-        "%d user likes this.": {
-            "plurals": [
-                "one",
-                "few",
-                "many",
-                "other"
-            ]
-        },
-        "One piggy went to the market.": {
-            "translation": "Одна свинья ушла на рынок."
-        }
-    },
-    "Button label": {
-        "Log in": {
-            "translation": "Войти"
-        }
-    },
-    "Context with plural": {
-        "One piggy went to the market.": {
-            "plurals": [
-                "Одна свинья ушла на рынок.",
-                "%d свиньи пошли на рынок.",
-                "На рынок вышли %d поросят.",
-                "%d поросят вышли на рынок."
-            ],
-            "translation": "Одна свинья ушла на рынок."
-        }
-    },
-    "Dialog title": {
-        "Log in": {
-            "translation": "Вход в систему"
-        }
-    },
-    "This is some context about the string.": {
-        "Accept language %{accept_language} was rejected": {
-            "translation": "Принять языки %{accept_language} были отклонены"
-        }
-    }
-}
-*/
-
 type MessageCatalog struct {
 	messages map[string]interface{}
 	mutex    sync.RWMutex
@@ -123,4 +72,41 @@ func (mc *MessageCatalog) GetMessages() (map[string]interface{}, error) {
 	}
 
 	return *messages, nil
+}
+
+func (mc *MessageCatalog) Gettext(msgid string) string {
+	mc.mutex.RLock()
+	defer mc.mutex.RUnlock()
+
+	msgctxtObj, ok := mc.messages[""]
+	if !ok {
+		return msgid
+	}
+
+	msgctxtMap, ok := msgctxtObj.(map[string]interface{})
+	if !ok {
+		return msgid
+	}
+
+	msgidObj, ok := msgctxtMap[msgid]
+	if !ok {
+		return msgid
+	}
+
+	msgidMap, ok := msgidObj.(map[string]interface{})
+	if !ok {
+		return msgid
+	}
+
+	msgstrObj, ok := msgidMap["translation"]
+	if !ok {
+		return msgid
+	}
+
+	msgstrStr, ok := msgstrObj.(string)
+	if !ok {
+		return msgid
+	}
+
+	return msgstrStr
 }
--- gettext_test.go
+++ gettext_test.go
@@ -11,6 +11,57 @@ const (
 	poFilePath = "testdata/test.po"
 )
 
+/*
+{
+    "": {
+        "": {
+            "Content-Transfer-Encoding": "8bit",
+            "Content-Type": "text/plain; charset=UTF-8",
+            "Language": "ru",
+            "MIME-Version": "1.0",
+            "Plural-Forms": "nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);"
+        },
+        "%d user likes this.": {
+            "plurals": [
+                "one",
+                "few",
+                "many",
+                "other"
+            ]
+        },
+        "One piggy went to the market.": {
+            "translation": "Одна свинья ушла на рынок."
+        }
+    },
+    "Button label": {
+        "Log in": {
+            "translation": "Войти"
+        }
+    },
+    "Context with plural": {
+        "One piggy went to the market.": {
+            "plurals": [
+                "Одна свинья ушла на рынок.",
+                "%d свиньи пошли на рынок.",
+                "На рынок вышли %d поросят.",
+                "%d поросят вышли на рынок."
+            ],
+            "translation": "Одна свинья ушла на рынок."
+        }
+    },
+    "Dialog title": {
+        "Log in": {
+            "translation": "Вход в систему"
+        }
+    },
+    "This is some context about the string.": {
+        "Accept language %{accept_language} was rejected": {
+            "translation": "Принять языки %{accept_language} были отклонены"
+        }
+    }
+}
+*/
+
 type TestSuite struct {
 	suite.Suite
 	mc *MessageCatalog
@@ -49,8 +100,19 @@ func (t *TestSuite) TestNewMessageCatalogFromBytes() {
 	t.NotNil(mc)
 }
 
-func (t *TestSuite) TestMessageCatalog_Gettext() {
+func (t *TestSuite) TestMessageCatalog_GetMessages() {
 	messages, err := t.mc.GetMessages()
 	t.NoError(err)
 	t.NotNil(messages)
+}
+
+func (t *TestSuite) TestMessageCatalog_Gettext_MsgidExists() {
+	msgstr := t.mc.Gettext("One piggy went to the market.")
+	t.EqualValues("Одна свинья ушла на рынок.", msgstr)
+}
+
+func (t *TestSuite) TestMessageCatalog_Gettext_MsgidMissing() {
+	msgid := "This msgid doesn't exist."
+	msgstr := t.mc.Gettext(msgid)
+	t.EqualValues(msgid, msgstr)
 }
